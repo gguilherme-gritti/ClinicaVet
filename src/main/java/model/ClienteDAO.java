ebeb7a060205075b1bc9f45037abd388725ace1a
gguilherme-gritti <78884167+gguilherme-gritti@users.noreply.github.com>
2022-09-19 19:28:55 -0300

    cliente dao

--- src/main/java/model/ClienteDAO.java
+++ src/main/java/model/ClienteDAO.java
@@ -6,6 +6,11 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,32 +18,96 @@ import java.util.List;
  */
 public class ClienteDAO extends DAO {
 
-    static List<Cliente> clientes = new ArrayList<>();
+    private static ClienteDAO instance;
 
-    public static List<Cliente> retrieveAll() {
+    private ClienteDAO() {
+        getConnection();
+        createTable();
+    }
+
+    public static ClienteDAO getInstance() {
+        return (instance == null ? (instance = new ClienteDAO()) : instance);
+    }
+
+    public Cliente create(int id_cli, int id_end, String nom_cli, String email_cli) {
+        try {
+            PreparedStatement stmt;
+            stmt = DAO.getConnection().prepareStatement("INSERT INTO Cliente (id_end, nom_cli, email_cli) VALUES (?,?,?)");
+            stmt.setInt(1, id_end);
+            stmt.setString(2, nom_cli);
+            stmt.setString(3, email_cli);
+            executeUpdate(stmt);
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return this.retrieveById(lastId("cliente", "id"));
+    }
+
+    public boolean isLastEmpty() {
+        Cliente lastClient = this.retrieveById(lastId("cliente", "id"));
+        if (lastClient != null) {
+            return lastClient.getNom_cli().isBlank();
+        }
+        return false;
+    }
+
+    private Cliente buildObject(ResultSet rs) {
+        Cliente cliente = null;
+        try {
+            cliente = new Cliente(rs.getInt("id_cli"), rs.getInt("id_end"), rs.getString("nom_cli"), rs.getString("email_cli"));
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
+        return cliente;
+    }
+
+    public List retrieve(String query) {
+        List<Cliente> clientes = new ArrayList();
+        ResultSet rs = getResultSet(query);
+        try {
+            while (rs.next()) {
+                clientes.add(buildObject(rs));
+            }
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
         return clientes;
     }
 
-    public static Cliente retrieveById(int id_cli) {
-        for(Cliente cli: clientes){
-            if(cli.getId_cli() == id_cli){
-                return cli;
-            }
+    public List retrieveLast() {
+        return this.retrieve("SELECT * FROM cliente WHERE id = " + lastId("cliente", "id"));
+    }
+
+    public Cliente retrieveById(int id) {
+        List<Cliente> clientes = this.retrieve("SELECT * FROM cliente WHERE id = " + id);
+        return (clientes.isEmpty() ? null : clientes.get(0));
+    }
+
+    public List retrieveBySimilarName(String nome) {
+        return this.retrieve("SELECT * FROM Cliente WHERE nome LIKE '%" + nome + "%'");
+    }
+
+    public void update(Cliente cliente) {
+        try {
+            PreparedStatement stmt;
+            stmt = DAO.getConnection().prepareStatement("UPDATE Cliente SET nom_cli=?, email_cli=? WHERE id=?");
+            stmt.setString(1, cliente.getNom_cli());
+            stmt.setString(2, cliente.getEmail_cli());
+            stmt.setInt(3, cliente.getId_cli());
+            executeUpdate(stmt);
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
         }
-        
-        return new Cliente(99, 99, "Cliente Não encontrado", "nao@encontrado.com");
     }
 
-    public static void create(int id_cli, int id_end, String nom_cli, String email_cli) {
-        clientes.add(new Cliente(id_cli, id_end, nom_cli, email_cli));
+    public void delete(Cliente cliente) {
+        PreparedStatement stmt;
+        try {
+            stmt = DAO.getConnection().prepareStatement("DELETE FROM Cliente WHERE id = ?");
+            stmt.setInt(1, cliente.getId_cli());
+            executeUpdate(stmt);
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
     }
-
-    public boolean update() {
-        return true;
-    }
-
-    public boolean delete() {
-        return true;
-    }
-
 }
